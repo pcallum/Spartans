698bc87cbadb7d3fa5be53e22c38499e32dd934d
pcallum <callumrp10@gmail.com>
2022-05-23 11:20:45 +0100

    Initial commit

---
+++ src/main/java/com/example/spartans/SpartansApplication.java
@@ -0,0 +1,13 @@
+package com.example.spartans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartansApplication.class, args);
+    }
+
+}
