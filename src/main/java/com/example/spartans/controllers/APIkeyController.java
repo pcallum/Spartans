d7b260040873d565d27300dd7aa078eeebae0b9b
maurosorrentino <sorrentino.mauro95@gmail.com>
2022-05-24 10:29:16 +0100

    gitignore fixed

--- src/main/java/com/example/spartans/controllers/APIkeyController.java
+++ src/main/java/com/example/spartans/controllers/APIkeyController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +25,8 @@ public class APIkeyController {
 
     private final String message = "{\"message\": \"";
 
-    private byte[] generateAPIkey() {
+    @GetMapping("/get-api-key")
+    public byte[] generateAPIkey() {
         KeyPairGenerator keyGen = null;
         try {
             keyGen = KeyPairGenerator.getInstance("RSA");
