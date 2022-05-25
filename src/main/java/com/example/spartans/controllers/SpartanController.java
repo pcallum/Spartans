392e2f8efcc91f31066e74852d52718384f4a62a
TinaSzucs <puncs17@gmail.com>
2022-05-25 11:23:16 +0100

    SpartanController RESTful URI names

--- src/main/java/com/example/spartans/controllers/SpartanController.java
+++ src/main/java/com/example/spartans/controllers/SpartanController.java
@@ -1,6 +1,5 @@
 package com.example.spartans.controllers;
 import com.example.spartans.entities.Spartan;
-import com.example.spartans.repositories.SpartanRepository;
 import com.example.spartans.service.SpartanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -8,42 +7,38 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/spartan")
+@RequestMapping("/api/spartans")
 public class SpartanController {
     @Autowired
     private SpartanService service;
-//    @Autowired
-//    SpartanRepository repository;
 
-    @PostMapping("/add")
+    @PostMapping()
     public Spartan addSpartan(@RequestBody Spartan spartan){
-//        return repository.save(spartan);
         return service.addSpartan(spartan);
     }
 
-    @GetMapping("/getAll")
+    @GetMapping()
     public List<Spartan> getAllSpartans() {
-//        return repository.findAll();
         return service.getAllSpartans();
     }
 
-    @GetMapping("/getById/{id}")
+    @GetMapping("/{id}")
     public Optional<Spartan> getSpartan(@PathVariable String id) {
-//        return repository.findById(id);
         return service.getSpartanById(id);
     }
 
-    @PutMapping("/update")
+    @PutMapping()
     public String updateSpartan(@RequestBody Spartan spartan){
         return service.updateSpartan(spartan);
     }
 
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping("/{id}")
     public String deleteSpartanById(@PathVariable String id){
         return service.deleteSpartanById(id);
     }
 
-    @GetMapping("/getByName/{name}")
+    // cannot be "/{name}" only, as you cant tell if the string refers to the id or name
+    @GetMapping("/name/{name}")
     public List<Spartan> getByPartialName(@PathVariable String name){
         return service.getSpartanByPartialName(name, name);
     }
