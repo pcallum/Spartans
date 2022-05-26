8b84be2f947354a1e1903f1e9c8d5d58279add7f
maurosorrentino <sorrentino.mauro95@gmail.com>
2022-05-26 14:56:06 +0100

    signup and hash password done

--- src/main/java/com/example/spartans/controllers/SignupController.java
+++ src/main/java/com/example/spartans/controllers/SignupController.java
@@ -1,4 +1,4 @@
-/* 
+
 package com.example.spartans.controllers;
 
 import java.util.Optional;
@@ -9,14 +9,12 @@ import com.example.spartans.repositories.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
 @RequestMapping("/api")
 public class SignupController {
     @Autowired
@@ -34,36 +32,41 @@ public class SignupController {
 
             if (optionalUser.isPresent()) {
                 res = ResponseEntity.status(422).body(
-                        this.message + "email " + email + " already exists\"");
+                        this.message + "email " + email + " already exists\"}");
                 return res;
             }
 
             if (signupRequest.getPassword().length() < 5) {
                 res = ResponseEntity.status(401).body(
-                        this.message + " password needs to be at least 5 characters\"");
+                        this.message + " password needs to be at least 5 characters\"}");
                 return res;
             }
 
-            if (!signupRequest.getRole().toLowerCase().equals("admin") ||
-                    !signupRequest.getRole().toLowerCase().equals("user")) {
+            // sonar lint: replace to lower case and equals with equals ignore case
+            if (!signupRequest.getRole().equalsIgnoreCase("admin") &&
+                    !signupRequest.getRole().equalsIgnoreCase("user")) {
                 res = ResponseEntity.status(401).body(
-                        this.message + " the role needs to be either admin or user");
+                        this.message + " the role needs to be either admin or user\"}");
                 return res;
             }
 
             User user = new User();
             user.setEmail(email);
-            user.setPassword(passwordEncoder.encode(signupRequest.getPassword()));
+
+            // hashing the password
+            String hashedPassword = HashPasswordController.generateHashPassword(signupRequest.getPassword());
+            user.setPassword(hashedPassword);
+            // saving role
             user.setRole(signupRequest.getRole().toLowerCase());
             userRepo.save(user);
 
             res = ResponseEntity.status(201).body(
-                    this.message + " user " + email + " was created\"");
+                    this.message + " user " + email + " was created\"}");
         } catch (Exception e) {
             res = ResponseEntity.status(500).body(
-                    this.message + "something went wrong\"");
+                    this.message + "something went wrong\"}");
             e.printStackTrace();
         }
         return res;
     }
-} */
+}
--- src/test/java/com/example/spartans/controllerTest/SpartanTest.java
+++ src/test/java/com/example/spartans/controllerTest/SpartanTest.java
@@ -1,68 +1,68 @@
-/* package com.example.spartans.ControllerTest;
-
-import com.example.spartans.controllers.SpartanController;
-import com.example.spartans.entities.Spartan;
-import com.example.spartans.repositories.SpartanRepository;
-import com.example.spartans.service.SpartanService;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import java.util.Optional;
-
-
-
-@SpringBootTest
-class SpartanTest {
-
-    @MockBean
-    SpartanRepository spartanRepository;
-
-    @MockBean
-    SpartanService spartanService;
-
-    @MockBean
-    SpartanController spartanController;
-
-
-    @Test
-    void checkSpartanIsFound() {
-        Spartan spartan = new Spartan();
-
-        Mockito.when(spartanRepository.findById("628caae9342e5fbc7c4bd383")).thenReturn(Optional.of(spartan));
-        Assertions.assertEquals(spartanRepository.findById("628caae9342e5fbc7c4bd383"), Optional.of(spartan));
-    }
-
-    @Test
-    void addSpartan() {
-        Spartan spartan = new Spartan();
-
-        spartan.setFirstName("michel");
-        spartan.setLastName("jean");
-        spartan.setCourse("c#");
-        spartan.setEmail("michel@gmail.com");
-        //spartan.setStartDate("05/12/1999");
-        spartanRepository.save(spartan);
-        Assertions.assertEquals("michel", spartan.getFirstName());
-    }
-
-    @Test
-    void checkSpartanIsEdited() {
-        Spartan spartan = new Spartan();
-
-        spartan.setId("628caae9342e5fbc7c4bd383");
-        spartan.setFirstName("Adriena updated");
-        spartanRepository.save(spartan);
-        Mockito.when(spartanRepository.findById("628caae9342e5fbc7c4bd383")).thenReturn(Optional.of(spartan));
-        Assertions.assertEquals("Adriena updated", spartan.getFirstName());
-    }
-
-    @Test
-    void checkSpartanIsRemoved() {
-        spartanRepository.deleteById("628caae9342e5fbc7c4bd383");
-        boolean result = spartanRepository.findById("628caae9342e5fbc7c4bd383").isEmpty();
-        Assertions.assertTrue(result);
-    }
-}
+/*
+ * package com.example.spartans.ControllerTest;
+ * 
+ * import com.example.spartans.controllers.SpartanController;
+ * import com.example.spartans.entities.Spartan;
+ * import com.example.spartans.repositories.SpartanRepository;
+ * import com.example.spartans.service.SpartanService;
+ * import org.junit.jupiter.api.Assertions;
+ * import org.junit.jupiter.api.Test;
+ * import org.mockito.Mockito;
+ * import org.springframework.boot.test.context.SpringBootTest;
+ * import org.springframework.boot.test.mock.mockito.MockBean;
+ * import java.util.Optional;
+ * 
+ * @SpringBootTest
+ * class SpartanTest {
+ * 
+ * @MockBean
+ * SpartanRepository spartanRepository;
+ * 
+ * @MockBean
+ * SpartanService spartanService;
+ * 
+ * @MockBean
+ * SpartanController spartanController;
+ * 
+ * @Test
+ * void checkSpartanIsFound() {
+ * Spartan spartan = new Spartan();
+ * 
+ * Mockito.when(spartanRepository.findById("628caae9342e5fbc7c4bd383")).
+ * thenReturn(Optional.of(spartan));
+ * Assertions.assertEquals(spartanRepository.findById("628caae9342e5fbc7c4bd383"
+ * ), Optional.of(spartan));
+ * }
+ * 
+ * @Test
+ * void addSpartan() {
+ * Spartan spartan = new Spartan();
+ * spartan.setFirstName("michel");
+ * spartan.setLastName("jean");
+ * spartan.setCourse("c#");
+ * spartan.setEmail("michel@gmail.com");
+ * spartan.setStartDate("05/12/1999");
+ * spartanRepository.save(spartan);
+ * Assertions.assertEquals("michel", spartan.getFirstName());
+ * }
+ * 
+ * @Test
+ * void checkSpartanIsEdited() {
+ * Spartan spartan = new Spartan();
+ * spartan.setId("628caae9342e5fbc7c4bd383");
+ * spartan.setFirstName("Adriena updated");
+ * spartanRepository.save(spartan);
+ * Spartan spartanUpdated =
+ * spartanRepository.findById("628caae9342e5fbc7c4bd383").get();
+ * Assertions.assertEquals("Adriena updated", spartanUpdated.getFirstName());
+ * }
+ * 
+ * @Test
+ * void checkSpartanIsRemoved() {
+ * spartanRepository.deleteById("628caae9342e5fbc7c4bd383");
+ * boolean result =
+ * spartanRepository.findById("628caae9342e5fbc7c4bd383").isEmpty();
+ * Assertions.assertTrue(result);
+ * }
+ * }
  */
